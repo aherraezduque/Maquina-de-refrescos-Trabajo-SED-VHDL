de65009b0af40e2cebbfd234959ff40f1794c1f9
aherraezduque <alvaro.herraez.duque@gmail.com>
2023-01-08 11:03:10 +0100

    Se añade el testbench de la entidad fsm. Se modifica el testbench de la entidad monedero.
    -Se añade el testbench de la entidad fsm (con GENERIC).
    -Se modifica el testbench de la entidad monedero (con GENERIC).

--- refrescos.srcs/sim_1/new/tb_monedero.vhd
+++ refrescos.srcs/sim_1/new/tb_monedero.vhd
@@ -20,50 +20,47 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity tb_monedero is
 
 end tb_monedero;
 
 architecture Behavioral of tb_monedero is
-    component monedero 
+    
+    component monedero
+        Generic (
+        valor_p10C: integer:= 10;
+        valor_p20C: integer:= 20;
+        valor_p50C: integer:= 50;
+        valor_p1_euro: integer:= 100;
+        valor_productos: integer:= 100;
+        valor_inicio: integer:= 0
+        ); 
         Port (
         CLK: in std_logic;              --CLK
         RESET: in std_logic;            --asynchronous active low reset
-        p10C: in std_logic;             --boton de 10cent
-        p20C: in std_logic;             --boton de 20cent
-        p50C: in std_logic;             --boton de 50cent
-        p1_euro: in std_logic;          --boton de 1 euro
+        p10C: in std_logic;             --botón de 10cent
+        p20C: in std_logic;             --botón de 20cent
+        p50C: in std_logic;             --botón de 50cent
+        p1_euro: in std_logic;          --botón de 1 euro
         saldo_monedero: out integer;    --saldo acumulado en el monedero
         aviso_devolucion: out std_logic --aviso en caso de sobrepasar los 100 cent == 1 euro
-             );
+        );
         end component;
-        --SIGNALS
-        --signal mi_monedero: integer :=0;
-        signal clk_s: std_logic:= '1';
-        constant tbPeriod: time :=1000 ns;
         
-        signal reset_s : std_logic;
+        --SIGNALS 
+        signal clk_s: std_logic:= '1';
+        constant tbPeriod: time :=10 ns;
+        
+        signal reset_s : std_logic:= '1';
         signal s_p10c: std_logic := '0';
         signal s_p20c: std_logic := '0';
         signal s_p50c: std_logic := '0';
         signal s_p1_euro: std_logic := '0';
         signal s_saldo_monedero: integer;
         signal s_aviso_devolucion: std_logic:= '0';
-        
-        
-        
-        
-        
+         
         TYPE vtest is record 
             reset_vt: std_logic;
             p10c_vt: std_logic;
@@ -77,31 +74,40 @@ architecture Behavioral of tb_monedero is
         TYPE vtest_vector IS ARRAY (natural RANGE<>) OF vtest;
         
     constant test: vtest_vector :=(
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--10cent        1
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>10, aviso_devolucion_vt=>'0'),--30cent       2
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>30, aviso_devolucion_vt=>'0'),--80cent       3
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>80, aviso_devolucion_vt=>'0'),--90cent       4
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>90, aviso_devolucion_vt=>'0'),--100cent      5
-             (reset_vt=>'0', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--reset asincrono   6
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --10cent                            1                                                                                        
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>10, aviso_devolucion_vt=>'0'),   --30cent                            2
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>30, aviso_devolucion_vt=>'0'),   --80cent                            3
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>80, aviso_devolucion_vt=>'0'),   --90cent                            4
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>90, aviso_devolucion_vt=>'0'),   --100cent                           5
+             (reset_vt=>'0', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --reset asíncrono                   6
              
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--10cent    7
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>10, aviso_devolucion_vt=>'0'),--30cent   8
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>30, aviso_devolucion_vt=>'0'),--80cent   9
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>80, aviso_devolucion_vt=>'0'),--90cent   10
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>90, aviso_devolucion_vt=>'0'),--100cent  11
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>100, aviso_devolucion_vt=>'0'),--100cent espera  12
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --10cent                            7
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>10, aviso_devolucion_vt=>'0'),   --30cent                            8
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>30, aviso_devolucion_vt=>'0'),   --80cent                            9
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>80, aviso_devolucion_vt=>'0'),   --90cent                            10
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>90, aviso_devolucion_vt=>'0'),   --100cent                           11
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>100, aviso_devolucion_vt=>'0'),  --100cent espera                    12
              
-             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>100, aviso_devolucion_vt=>'0'),--100cent y me paso al pulsar p10c    13
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'1'),--ha saltado el aviso   14
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'1', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'), --1euro    15
-             (reset_vt=>'0', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--reset asincrono   16
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--nada  17
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),--nada 20cent + 50cent  18
-             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>70, aviso_devolucion_vt=>'0')--nada      19
+             (reset_vt=>'1', p10c_vt=>'1', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>100, aviso_devolucion_vt=>'0'),  --100cent y me paso al pulsar p10c  13
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'1'),    --ha saltado el aviso               14
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'1', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --1euro                             15
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>100, aviso_devolucion_vt=>'0'),  --saldo 1euro y espero              16
+             (reset_vt=>'0', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --reset asíncrono                   17
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --nada                              18
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'1', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>0, aviso_devolucion_vt=>'0'),    --nada 20cent                       19
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'1', p1_euro_vt=>'0', saldo_monedero_vt=>20, aviso_devolucion_vt=>'0'),   --50cent                            20
+             (reset_vt=>'1', p10c_vt=>'0', p20c_vt=>'0', p50c_vt=>'0', p1_euro_vt=>'0', saldo_monedero_vt=>70, aviso_devolucion_vt=>'0')    --saldo 70cent y espero             21
              );
              
 begin
-    uut: monedero 
+    uut: monedero
+    generic map (
+        valor_p10C => 10,
+        valor_p20C => 20,
+        valor_p50C => 50,
+        valor_p1_euro => 100,
+        valor_productos => 100,
+        valor_inicio => 0 )
     port map (CLK => clk_s,
               RESET => reset_s,
               p10C => s_p10c,          
@@ -123,11 +129,11 @@ begin
         s_p20c <= test(i).p20c_vt;
         s_p50c <= test(i).p50c_vt;
         s_p1_euro <= test(i).p1_euro_vt;
-        wait for tbPeriod;                                  --Comprobacion de salidas
-        assert s_saldo_monedero = test(i).saldo_monedero_vt --¿es correcto el saldo?
+        wait for tbPeriod;                                      --Comprobacion de salidas
+        assert s_saldo_monedero = test(i).saldo_monedero_vt     --¿es correcto el saldo?
                report "Salida incorrecta -saldo"
                severity FAILURE ;
-        assert s_aviso_devolucion = test(i).aviso_devolucion_vt--¿saltó el aviso de devolucion?      
+        assert s_aviso_devolucion = test(i).aviso_devolucion_vt --¿saltó el aviso de devolucion?      
                report "Salida incorrecta -aviso"
                severity FAILURE ;
     end loop;
